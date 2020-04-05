c8d5879862bc4f04396dc9cf7cc3b9843eee6aad
Daniel Brownlees <daniel@osel.nz>
2020-04-06 11:50:53 +1200

    Add PWA instructions

--- src/help/index.tsx
+++ src/help/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTimes } from '@fortawesome/pro-regular-svg-icons'
+import {
+    faTimes,
+    faSignOut,
+    faEllipsisV,
+} from '@fortawesome/pro-regular-svg-icons'
+import { faPlusSquare } from '@fortawesome/pro-light-svg-icons'
 import { useContextState } from '../app'
 import { toggleHelp } from '../timer/actions'
 
@@ -12,6 +17,7 @@ export const Help: React.FC = () => {
         <div className={styles.paper}>
             <div className={styles.header}>
                 <img src="logo192.png" title="Logo" />
+                <a href="mailto:contact@simplemeditationtimer.com">Contact</a>
                 <button title="Close" onClick={() => dispatch(toggleHelp())}>
                     <FontAwesomeIcon icon={faTimes} />
                 </button>
@@ -23,9 +29,30 @@ export const Help: React.FC = () => {
                     good.
                 </p>
                 <p>
-                    <a href="mailto:contact@simplemeditationtimer.com">
-                        Contact
-                    </a>
+                    To save as an offline app:
+                    <dl>
+                        <dt>
+                            <strong>iOS</strong>
+                        </dt>
+                        <dd>
+                            From the share menu [{' '}
+                            <FontAwesomeIcon rotation={270} icon={faSignOut} />{' '}
+                            ] select{' '}
+                            <em>
+                                Add to Home Screen&nbsp;&nbsp;
+                                <FontAwesomeIcon icon={faPlusSquare} />
+                            </em>{' '}
+                        </dd>
+                        <br />
+                        <dt>
+                            <strong>Android</strong>
+                        </dt>
+                        <dd>
+                            From the Chrome menu [{' '}
+                            <FontAwesomeIcon icon={faEllipsisV} /> ] select{' '}
+                            <em>Add to Home screen</em>
+                        </dd>
+                    </dl>
                 </p>
             </div>
             <div className={styles.footer}>
